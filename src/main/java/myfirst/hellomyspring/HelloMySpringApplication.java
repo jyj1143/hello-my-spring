bdb95d9af7605ea7d1df80464b2449e1c5f80702
jyj1143 <jyj1143@gmail.com>
2022-09-01 15:42:26 +0900

    static contents commit

---
+++ src/main/java/myfirst/hellomyspring/HelloMySpringApplication.java
@@ -0,0 +1,13 @@
+package myfirst.hellomyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myfirst/hellomyspring/HelloMySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirst.hellomyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
