170904903d20ba3d83e7bc05794d9ac935593346
jyj1143 <jyj1143@gmail.com>
2022-09-03 21:51:36 +0900

    Compoenet Scan and Dependecy Injection

--- src/main/java/myfirst/hellomyspring/service/MemberService.java
+++ src/main/java/myfirst/hellomyspring/service/MemberService.java
@@ -3,13 +3,17 @@ package myfirst.hellomyspring.service;
 import myfirst.hellomyspring.domain.Member;
 import myfirst.hellomyspring.repository.MemberRepository;
 import myfirst.hellomyspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository=memberRepository;
     }
